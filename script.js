48bd09f36097db3fc905cff00f8c582a73cd1e1d
Soujanya A R <soujanyaar21@gmail.com>
2026-05-16 18:58:56 +0530

    Add HTML content for semester display

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function showSemester (sem,btn){
     let content = "";
     switch (sem){
         case 1:
+            content = `   
+            <h2 id="semester">semester 1</h2>
+            <h3 id="semestet-c">CGPA:7.87</h3>
