da7d7ece8d900972214beedff0926cb8b39b51b6
Soujanya A R <soujanyaar21@gmail.com>
2026-05-17 20:14:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function showSemester (sem,btn){
     let content = "";
     switch (sem){
-        case 1:
+        case 1: 
             content = `   
             <h2 id="semester">semester 1</h2>
             <h3 id="semestet-c">CGPA:7.87</h3>
